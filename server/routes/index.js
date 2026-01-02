7ebe2eff6410dfd6590eaf8317174bd69e0b8c40
skandraajb <skandraajb@gmail.com>
2026-01-02 21:22:48 +0530

    modularzied complete server

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,7 +1,5 @@
 const hrRouter = require('./hr');
-// const gdRouter = require('./gd');  // Teammate adds this line later
 
 module.exports = (app) => {
   app.use('/api', hrRouter);
-  // app.use('/api', gdRouter);  // Auto-mounts when added
 };

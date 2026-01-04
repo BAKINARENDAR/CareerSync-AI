1fbdc2b1355520d3efcf37da06705eaae95c6a27
Baki Narendar <bakinarendar@gmail.com>
2026-01-04 17:43:01 +0530

    index fixed

--- server/routes/index.js
+++ server/routes/index.js
@@ -2,6 +2,6 @@ const hrRouter = require('./hr');
 const technicalRouter = require('./technical');
 
 module.exports = (app) => {
-  app.use('/api/hr', hrRouter);
+  app.use('/api', hrRouter);
  app.use('/api/technical', technicalRouter);
 };
